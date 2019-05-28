9ec4002d734227a14cbce0d265f3aad649dcf107
Pierre Rousseau <rousseau@mercenaries-engineering.com>
2019-05-28 16:32:03 +0200

    Remove some libs available through conan from GuerillaBinaries

--- update_guerilla_recipes.sh
+++ update_guerilla_recipes.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 conan export blosc_1.11.2/conanfile.py blosc/1.11.2@pierousseau/stable
+conan export GuerillaBinaries_1.0/conanfile.py GuerillaBinaries/1.0@pierousseau/stable
+conan export GuerillaBinaries_1.1/conanfile.py GuerillaBinaries/1.1@pierousseau/stable
 conan export hdf5_1.10.1/conanfile.py hdf5/1.10.1@pierousseau/stable
 conan export libjpeg-turbo_1.5.2/conanfile.py libjpeg-turbo/1.5.2@pierousseau/stable
 conan export nasm_2.13.01/conanfile.py nasm/2.13.01@pierousseau/stable
