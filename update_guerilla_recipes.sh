5ba8997c9c4739aaaf77739f4ee9523223c10544
Pierre Rousseau <rousseau@mercenaries-engineering.com>
2019-07-01 11:22:33 +0200

    OpenColorIO 1.1.1

--- update_guerilla_recipes.sh
+++ update_guerilla_recipes.sh
@@ -12,7 +12,8 @@ conan export libjpeg-turbo_1.5.2/conanfile.py libjpeg-turbo/1.5.2@pierousseau/st
 conan export llvm_guerillaBinaries_3.5.1/conanfile.py llvm/guerillaBinaries_3.5.1@pierousseau/stable
 conan export llvm_3.5.1/conanfile.py llvm/3.5.1@pierousseau/stable
 conan export nasm_2.13.01/conanfile.py nasm/2.13.01@pierousseau/stable
-conan export opencolorio_guerillaBinaries_1.0.8/conanfile.py opencolorio/guerillaBinaries_1.0.8@pierousseau/stable
+#conan export opencolorio_guerillaBinaries_1.0.8/conanfile.py opencolorio/guerillaBinaries_1.0.8@pierousseau/stable
+conan export OpenColorIO_1.1.1/conanfile.py OpenColorIO/1.1.1@pierousseau/stable
 conan export OpenEXR_2.2.0/conanfile.py OpenEXR/2.2.0@pierousseau/stable
 conan export OpenExrId_1.0-beta.11/conanfile.py OpenExrId/1.0-beta.11@pierousseau/stable
 conan export OpenImageDenoise_0.9.0/conanfile.py OpenImageDenoise/0.9.0@pierousseau/stable
--- upload_guerilla_recipes.sh
+++ upload_guerilla_recipes.sh
@@ -5,17 +5,18 @@ conan user -p 94cb8468316779a44cdd6d3666d64fd3e4864cbc -r pierousseau pieroussea
 
 conan upload Alembic/1.7.3@pierousseau/stable -r pierousseau --all
 conan upload blosc/1.11.2@pierousseau/stable -r pierousseau --all
-conan upload embree/guerillaBinaries_3.2.0@pierousseau/stable -r pierousseau --all
+#conan upload embree/guerillaBinaries_3.2.0@pierousseau/stable -r pierousseau --all
 conan upload embree/3.5.2@pierousseau/stable -r pierousseau --all
 conan upload fumefx/guerillaBinaries_4@pierousseau/stable -r pierousseau --all
 #conan upload GuerillaBinaries/1.0@pierousseau/stable -r pierousseau --all
 conan upload hdf5/1.10.1@pierousseau/stable -r pierousseau --all
 conan upload jemalloc/4.3.1@pierousseau/stable -r pierousseau --all
 conan upload libjpeg-turbo/1.5.2@pierousseau/stable -r pierousseau --all
-conan upload llvm/guerillaBinaries_3.5.1@pierousseau/stable -r pierousseau --all
+#conan upload llvm/guerillaBinaries_3.5.1@pierousseau/stable -r pierousseau --all
 conan upload llvm/3.5.1@pierousseau/stable -r pierousseau --all
 conan upload nasm/2.13.01@pierousseau/stable -r pierousseau --all
-conan upload opencolorio/guerillaBinaries_1.0.8@pierousseau/stable -r pierousseau --all
+#conan upload opencolorio/guerillaBinaries_1.0.8@pierousseau/stable -r pierousseau --all
+conan upload OpenColorIO/1.1.1@pierousseau/stable -r pierousseau --all
 conan upload OpenEXR/2.2.0@pierousseau/stable -r pierousseau --all
 conan upload OpenExrId/1.0-beta.11@pierousseau/stable -r pierousseau --all
 conan upload OpenImageDenoise/0.9.0@pierousseau/stable -r pierousseau --all
