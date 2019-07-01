194a8230a5909eaeecb6b8605881ee79ca8c5dca
Pierre Rousseau <rousseau@mercenaries-engineering.com>
2019-07-01 09:48:26 +0200

    Embree 3.5.2 for windows.

--- embree_3.5.2/conanfile.py
+++ embree_3.5.2/conanfile.py
@@ -24,12 +24,21 @@ class EmbreeConan(ConanFile):
         tools.untargz(filename)
         os.unlink(filename)
 
+        tools.replace_in_file("embree-%s/common/cmake/FindTBB.cmake" % self.version,
+            "FIND_LIBRARY(TBB_LIBRARY tbb",
+            """FIND_LIBRARY(TBB_LIBRARY NAMES tbb tbb_debug""")
+        tools.replace_in_file("embree-%s/common/cmake/FindTBB.cmake" % self.version,
+            "FIND_LIBRARY(TBB_LIBRARY_MALLOC tbbmalloc",
+            """FIND_LIBRARY(TBB_LIBRARY_MALLOC NAMES tbbmalloc tbbmalloc_debug""")
+
     def build(self):
         cmake = CMake(self)
+        cmake.definitions["EMBREE_TUTORIALS"] = False
         if ("fPIC" in self.options.fields and self.options.fPIC == True):
             cmake.definitions["CMAKE_POSITION_INDEPENDENT_CODE"] = True
         cmake.definitions["TBB_ROOT"] = self.deps_cpp_info["TBB"].rootpath
-        cmake.definitions["EMBREE_TUTORIALS"] = False
+        if self.settings.os == "Windows" and self.settings.build_type == "Debug":
+            cmake.definitions["TBB_LIBRARY"] = self.deps_cpp_info["TBB"].rootpath
         cmake.configure(source_dir="embree-%s" % self.version)
 
         #cmake.build()
@@ -38,10 +47,10 @@ class EmbreeConan(ConanFile):
 
     def package(self):
         self.copy("*.h", src="embree-%s/" % self.version, dst="include/embree/")
-        self.copy("libmath.a", dst="lib", keep_path=False)
-        self.copy("libsimd.a", dst="lib", keep_path=False)
-        self.copy("math.lib", dst="lib", keep_path=False)
-        self.copy("simd.lib", dst="lib", keep_path=False)
+        self.copy("*/libmath.a", dst="lib", keep_path=False)
+        self.copy("*/libsimd.a", dst="lib", keep_path=False)
+        self.copy("*/math.lib", dst="lib", keep_path=False)
+        self.copy("*/simd.lib", dst="lib", keep_path=False)
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
