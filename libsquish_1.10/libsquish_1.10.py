11be691cde7882665f293a358bf0194155ac4a26
Cyril Corvazier <corvazier@mercs-eng.com>
2020-11-09 01:34:40 +0100

    [Add] libsquish windows

---
+++ libsquish_1.10/libsquish_1.10.py
@@ -0,0 +1,64 @@
+from distutils.spawn import find_executable
+from conans import ConanFile, CMake, tools
+import os, stat, shutil
+
+def on_shutil_rmtree_error( func, path, exc_info ):
+    os.chmod( path, stat.S_IWRITE )
+    os.remove( path )
+
+def remove_directory( directory_path ):
+    shutil.rmtree( directory_path, onerror = on_shutil_rmtree_error )
+
+def remove( path ):
+    if os.path.exists( path ):
+        if os.path.isdir( path ):
+            remove_directory( path )
+        else:
+            os.remove( path )
+
+
+class LibSquish( ConanFile ):
+    name = "libsquish"
+    license = " "
+    description = "C library for reading and writing files containing samples audio data"
+    url = "https://github.com/svn2github/libsquish"
+    generators = "virtualrunenv"
+
+    version = "1.10"
+    settings = "os", "arch", "compiler", "build_type"
+    options = { "shared": [ True, False ] }
+    default_options = { "shared": False }
+    _source_subfolder = "source_subfolder"
+
+    def source( self ):
+        sha = "c763145a30512c10450954b7a2b5b3a2f9a94e00"
+        download_url = "https://github.com/svn2github/libsquish/archive/{}.zip".format( sha )
+        folder_name = "libsquish-{}".format( sha )
+        zipped_folder_name = "{}.zip".format( folder_name )
+
+        tools.download( download_url, zipped_folder_name )
+        tools.unzip( zipped_folder_name )
+        shutil.move( folder_name, self._source_subfolder )
+        os.remove( zipped_folder_name )
+
+    def build( self ):
+        cmake = CMake(self)
+        cmake.definitions['CMAKE_BUILD_TYPE'] = "Release"
+
+        definition_dict = {
+            "CMAKE_BUILD_TYPE": "Release"
+        }
+
+        cmake.configure(defs = definition_dict, source_dir=self._source_subfolder)
+        cmake.build()
+        cmake.install()
+
+    def package( self ):
+        #remove( "{}/share".format( self.package_folder ) )
+        #remove( "{}/lib/pkgconfig".format( self.package_folder ) )
+        pass
+
+    def package_info( self ):
+        self.env_info.LD_LIBRARY_PATH.append(os.path.join(self.package_folder, "lib"))
+        self.cpp_info.libs = tools.collect_libs(self)
+
