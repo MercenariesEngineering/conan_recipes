ef55deaecac9d65dd7bbad23a3c7872e79b95c7d
Tom Delame <tdelame@gmail.com>
2021-02-11 10:50:11 +0100

    [Fix] missing HDX_API in Usd

--- upload_shared_recipes.sh
+++ upload_shared_recipes.sh
@@ -78,7 +78,7 @@
 #conan upload unistd/1.0@mercseng/v0                                             -r mercseng --all --confirm
 #conan upload USD/20.05@mercseng/v4                                              -r mercseng --all --confirm
 #conan upload USD/20.11@mercseng/v1                                              -r mercseng --all --confirm
-# conan upload USD/20.11-rumba@mercseng/v2                                        -r mercseng --all --confirm
+#conan upload USD/20.11-rumba@mercseng/v3                                        -r mercseng --all --confirm
 #conan upload winflexbison/2.5.22@mercseng/v0                                    -r mercseng --all --confirm
 #conan upload wxwidgets/2.8.12@mercseng/v0                                       -r mercseng --all --confirm
 #conan upload zlib/1.2.11@mercseng/v0                                            -r mercseng --all --confirm
