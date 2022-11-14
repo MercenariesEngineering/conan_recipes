bbfc658053d8753eb3af45e9145f9f472e789467
Cyril Corvazier <corvazier@mercs-eng.com>
2022-11-14 22:04:24 +0100

    [Add] ANTLR 4.8.1

---
+++ antlr_4.8.1/conanfile.py
@@ -0,0 +1,39 @@
+from conans import ConanFile, CMake
+import os
+
+class AntlrConan(ConanFile):
+    name = "antlr"
+    version = "4.8.1"
+    license = "BSD"
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False]}
+    default_options = {"shared": False}
+    generators = "cmake"
+    exports_sources = "src/*"
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure(source_folder="src")
+        cmake.build()
+
+        # Explicit way:
+        # self.run('cmake "%s/src" %s' % (self.source_folder, cmake.command_line))
+        # self.run("cmake --build . %s" % cmake.build_config)
+
+    def package(self):
+        self.copy("*.h", dst="include", src="src/runtime/src")
+        self.copy("*.lib", dst="lib", keep_path=False)
+        self.copy("*.dll", dst="bin", keep_path=False)
+        self.copy("*.dylib*", dst="lib", keep_path=False)
+        self.copy("*.so", dst="lib", keep_path=False)
+        self.copy("*.a", dst="lib", keep_path=False)
+        self.copy("*.jar", dst="class", src="src/class")
+
+    def package_info(self):
+        self.cpp_info.libs = ["antlr4-runtime"]
+        if self.options.shared:
+            if self.settings.os == "Windows":
+                self.env_info.PATH.append(os.path.join(self.package_folder, "bin"))
+            else:
+                self.env_info.LD_LIBRARY_PATH.append(os.path.join(self.package_folder, "lib"))
+        self.env_info.CLASSPATH.append(os.path.join(self.package_folder, "class/antlr-4.8-complete.jar"))
