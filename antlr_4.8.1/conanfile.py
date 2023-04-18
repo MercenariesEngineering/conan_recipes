87a3f61921db634a413b1f2d15ba474dc712c9f8
Cyril Corvazier <corvazier@mercs-eng.com>
2023-04-18 16:09:21 +0200

    [Fix] Enable antlr static,shared,fPic builds

--- antlr_4.8.1/conanfile.py
+++ antlr_4.8.1/conanfile.py
@@ -6,10 +6,14 @@ class AntlrConan(ConanFile):
     version = "4.8.1"
     license = "BSD"
     settings = "os", "compiler", "build_type", "arch"
-    options = {"shared": [True, False]}
-    default_options = {"shared": False}
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = {"shared": False, "fPIC": True}
     generators = "cmake"
     exports_sources = "src/*"
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
 
     def requirements(self):
         if self.settings.os == "Linux":
@@ -17,8 +21,9 @@ class AntlrConan(ConanFile):
 
     def build(self):
         cmake = CMake(self)
-        cmake.configure(source_folder="src")
-        cmake.build()
+        definition_dict = {"WITH_STATIC_CRT":"Off"}
+        cmake.configure(source_folder="src", defs=definition_dict)
+        cmake.build(target = "antlr4_shared" if self.options.shared else "antlr4_static")
 
         # Explicit way:
         # self.run('cmake "%s/src" %s' % (self.source_folder, cmake.command_line))
@@ -34,10 +39,13 @@ class AntlrConan(ConanFile):
         self.copy("*.jar", dst="class", src="src/class")
 
     def package_info(self):
-        self.cpp_info.libs = ["antlr4-runtime"]
+        self.cpp_info.libs = ["antlr4-runtime-static" if self.settings.os == "Windows" else "antlr4-runtime"]
         if self.options.shared:
             if self.settings.os == "Windows":
                 self.env_info.PATH.append(os.path.join(self.package_folder, "bin"))
             else:
                 self.env_info.LD_LIBRARY_PATH.append(os.path.join(self.package_folder, "lib"))
+        else:
+            if self.settings.os == "Windows":
+                self.cpp_info.defines.append("ANTLR4CPP_STATIC")
         self.env_info.CLASSPATH.append(os.path.join(self.package_folder, "class/antlr-4.8-complete.jar"))
