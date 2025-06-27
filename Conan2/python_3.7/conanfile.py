d3ba1254486391e2966c6bd3f211fa0a9dceb7c9
Pierre Rousseau <rousseau@mercenaries-engineering.com>
2025-06-27 18:30:38 +0200

    [Conan2] Header-only recipes for Python2.6 and Python3.7, used by Guerilla2.

---
+++ Conan2/python_3.7/conanfile.py
@@ -0,0 +1,22 @@
+from conan import ConanFile
+from conan.tools.files import copy, collect_libs
+import os
+
+class pythonConan(ConanFile):
+    name = "python3-headers"
+    version = "3.7"
+    user="mercs"
+    settings = "os"
+    description = "Python is a programming language that lets you work quickly and integrate systems more effectively"
+    url = "https://www.python.org/"
+    exports_sources = "dist/*"
+
+    def package(self):
+        if self.settings.os == "Windows" :
+            copy(self, "*.h", src=os.path.join(self.source_folder, "dist", "windows"), dst=os.path.join(self.package_folder, "include"))
+        elif self.settings.os == "Linux" :
+            copy(self, "*.h", src=os.path.join(self.source_folder, "dist", "linux"), dst=os.path.join(self.package_folder, "include"))
+
+    def package_info(self):
+        self.cpp_info.set_property("cmake_file_name", "python3-headers")
+        self.cpp_info.set_property("cmake_target_name", "Mercs::python3-headers")
